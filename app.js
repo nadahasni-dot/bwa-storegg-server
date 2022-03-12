cc7574395b50fb2c3b0a7f10ae65accd82eb4deb
Nada Hasni <nadasthing@gmail.com>
2022-03-12 16:49:13 +0700

    add edit data category page and action

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const methodOverride = require("method-override");
 
 var dashboardRouter = require("./app/dashboard/router");
 var categoryRouter = require("./app/category/router");
@@ -13,12 +14,16 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/adminlte', express.static(path.join(__dirname, "/node_modules/admin-lte/")));
+app.use(
+  "/adminlte",
+  express.static(path.join(__dirname, "/node_modules/admin-lte/"))
+);
 
 app.use("/", dashboardRouter);
 app.use("/category", categoryRouter);
