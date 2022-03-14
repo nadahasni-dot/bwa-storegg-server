c2b4a5989c63ab0d638a83020e2b056808410bb6
Nada Hasni <nadasthing@gmail.com>
2022-03-14 15:11:02 +0700

    add logout functionality

--- app/user/controller.js
+++ app/user/controller.js
@@ -65,65 +65,15 @@ module.exports = {
       res.redirect("/");
     }
   },
-  // viewEdit: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
-  //     const nominal = await Nominal.findOne({ _id: id });
+  actionLogout: (req, res) => {
+    try {
+      req.session.destroy();
+      res.redirect("/");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
 
-  //     const alertMessage = req.flash("alertMessage");
-  //     const alertStatus = req.flash("alertStatus");
-  //     const alert = { message: alertMessage, status: alertStatus };
-
-  //     res.render("admin/nominal/edit", { nominal, alert });
-  //   } catch (error) {
-  //     console.log(error);
-  //     req.flash("alertMessage", `${error.message}`);
-  //     req.flash("alertStatus", `danger`);
-
-  //     console.log(error);
-  //     res.redirect("/nominal/edit");
-  //   }
-  // },
-  // actionEdit: async (req, res) => {
-  //   const { id } = req.params;
-
-  //   try {
-  //     const { coinName, coinQuantity, price } = req.body;
-
-  //     await Nominal.findOneAndUpdate(
-  //       { _id: id },
-  //       { coinName, coinQuantity, price }
-  //     );
-
-  //     req.flash("alertMessage", "Berhasil update data");
-  //     req.flash("alertStatus", "success");
-
-  //     res.redirect("/nominal");
-  //   } catch (error) {
-  //     console.log(error);
-  //     req.flash("alertMessage", `${error.message}`);
-  //     req.flash("alertStatus", `danger`);
-
-  //     res.redirect(`/nominal/edit/${id}`);
-  //   }
-  // },
-  // actionDelete: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
-
-  //     await Nominal.deleteOne({ _id: id });
-
-  //     req.flash("alertMessage", "Berhasil hapus data");
-  //     req.flash("alertStatus", "success");
-
-  //     res.redirect("/nominal");
-  //   } catch (error) {
-  //     console.log(error);
-  //     req.flash("alertMessage", `${error.message}`);
-  //     req.flash("alertStatus", `danger`);
-
-  //     console.log(error);
-  //     res.redirect("/nominal");
-  //   }
-  // },
+      res.redirect("/");
+    }
+  },
 };
