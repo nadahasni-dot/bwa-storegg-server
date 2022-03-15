61a47c2c5730c0e59c47f3877d339e5726be398d
Nada Hasni <nadasthing@gmail.com>
2022-03-15 10:28:02 +0700

    add get all categories api

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -30,6 +31,19 @@ module.exports = {
       }
 
       res.status(200).json({ status: true, message: "success", data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  },
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+
+      res
+        .status(200)
+        .json({ status: true, message: "success", data: category });
     } catch (error) {
       res
         .status(500)
